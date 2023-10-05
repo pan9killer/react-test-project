03f5a53e008a77ef26e0777859418516d3cb1777
p.severin <p.severin@uds.systems>
2023-10-05 12:30:35 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const TodoItem = ({ item, done }) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', fontSize: '26px' }}>
+      <span>{item}</span>
+      <button onClick={() => done(item)}>done</button>
+    </div>
+  );
+};
+
+const AddNewItem = ({ addNewTodo }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const changeValue = e => {
+    e.preventDefault();
+    if (!!e.target.value) {
+      setInputValue(e.target.value);
+    }
+  };
+
+  return (
+    <div>
+      <h6>Add new TODO:</h6>
+      <input type='text' value={inputValue} onChange={e => changeValue(e)} />
+      <button
+        onClick={() => {
+          addNewTodo(inputValue);
+          setInputValue('');
+        }}
+      >
+        add
+      </button>
+    </div>
+  );
+};
 
 function App() {
+  const [todoList, setTodoList] = useState(['eat', 'sleep', 'work']);
+
+  const done = value => {
+    const newTodo = todoList.filter(item => item !== value);
+    setTodoList(newTodo);
+  };
+
+  const addNewTodo = value => {
+    setTodoList(prev => [...prev, value]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <AddNewItem addNewTodo={addNewTodo} />
+        {todoList.map(item => (
+          <TodoItem key={item} item={item} done={done} />
+        ))}
       </header>
     </div>
   );
